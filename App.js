dd41d31250dab4fd9399a722e83a03686b5a2665
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-04 18:13:13 +0530

    Adding different components in React Native using Expo and CRNA

---
+++ App.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+import Hello from './Hello';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Hello />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+// //App.js- Place code here for android
+//
+// //import a library to help create a Component
+// import React frpm 'react';
+// import { Text, AppRegistry } from 'react-native';
+//
+// //Create a Component
+// const App = () => {
+//   return(
+//     <Text>Some Text</Text>
+//   );
+// };
+//
+// //Render it to a device
+// AppRegistry.registerComponent('albums', () => App);
---
+++ Hello.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default class Hello extends React.Component{
+  render() {
+    return(
+      <View>
+        <Text>Hello Srivatsa creating another component</Text>
+      </View>
+    )
+  }
+}
