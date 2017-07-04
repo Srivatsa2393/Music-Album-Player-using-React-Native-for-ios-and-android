318c3498bb3e406dbca030da53fa5ad914882d95
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-04 19:39:48 +0530

    more on styling used View to make it display in the center

--- src/components/header.js
+++ src/components/header.js
@@ -1,21 +1,26 @@
 //import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 
 //Make a component
 class Header extends React.Component{
   render() {
-    const { textStyle } = styles;
+    const { textStyle, viewStyle } = styles;
     return(
-      <Text style={textStyle}>
-        Albums!
-      </Text>
+      <View style={viewStyle}>
+        <Text style={textStyle}>
+          Albums!
+        </Text>
+      </View>
     );
   }
 }
 
 const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8'
+  },
   textStyle: {
     fontSize: 20
   }
