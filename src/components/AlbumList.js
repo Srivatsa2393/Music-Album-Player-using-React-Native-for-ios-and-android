36fa013945254d651066670a2c509c1e3026e9a6
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-05 14:31:41 +0530

    adding state to AlbumList to fetch the data from axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
 class AlbumList extends Component{
+  state = { albums: [] };
+
 componentWillMount() {
-  console.log('Component will mount in AlbumList');
+  //console.log('Component will mount in AlbumList');
+  axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+    .then(response => this.setState({ albums: response.data }));
 }
 
   render() {
+    console.log(this.state);
     return(
       <View>
         <Text>Album List!!!</Text>
