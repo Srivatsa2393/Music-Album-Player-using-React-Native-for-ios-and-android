65cffb31033459a51156478f3763893115e2de3a
Srivatsa S <srivatsashankaran@gmail.com>
2017-07-05 21:15:05 +0530

    Added the album artwork image by adding another CardSection and styling it

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,11 +7,22 @@ const AlbumDetail = (props) => {
   return(
     <Card>
       <CardSection>
-        <View></View>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: props.album.thumbnail_image }}
+          />
+        </View>
         <View style={styles.headerContentStyle}>
-          <Text>{props.album.title}</Text>
+          <Text style={styles.headerTextStyle}>{props.album.title}</Text>
           <Text>{props.album.artist}</Text>
         </View>
+      </CardSection>
+      <CardSection>
+        <Image
+          style={styles.imageStyle}
+          source={{ uri: props.album.image }}
+        />
       </CardSection>
     </Card>
   );
@@ -21,6 +32,24 @@ const styles = {
   headerContentStyle: {
     flexDirection: 'column',
     justifyContent: 'space-around'
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center'.
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
